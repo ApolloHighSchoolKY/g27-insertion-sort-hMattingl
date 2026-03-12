846e509f4e70029d920bc902e1fc2e374e69bc0c
hMattingl <hayden.mattingly2@stu.daviess.kyschools.us>
2026-03-12 02:46:11 +0000

    Commit

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
@@ -5,5 +7,32 @@ public class AInsertionSort
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
 
+        //temp for storing the val to place into array
+        int temp = 0;
+
+        //starting at the 2nd num, check ending at the last pos in the array
+        for(int current = 1; current < myNumbers.length; current ++)
+        {
+            //checking all behind the current val of the out loop
+            for(int check = 0; check < current; check ++)
+            {
+                // if the current val is less than a val behind it
+                if(myNumbers[current] < myNumbers[check])
+                {
+                    //save that val, move the ones in between ahead one, and place the val in the spot it would be in assending order
+                    temp = myNumbers[current];
+                    for(int replace = current; replace > check; replace--)
+                    {
+                        
+                        myNumbers[replace] = myNumbers[replace - 1];
+
+                    }
+                    myNumbers[check] = temp;
+                }
+                     
+            }
+            //print for testing 
+            System.out.println(Arrays.toString(myNumbers));
+        }
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -10,24 +11,43 @@ public class ALInsertionSort
     	boolean inserted=false;
 
 		//Add the first number to the array list
+		sorted.add(myNumbers[0]);
 
 		//Loop once for all of the remaining numbers in the unsorted list
+		for(int remain = 1; remain < myNumbers.length ; remain ++)
+		{
+			//reset boolen for reuse
+			inserted = false;
 
 			//Check with each of the numbers in the sorted list
-
+			for(int check = 0; check < remain; check++)
+			{
 				//If this number is less than one in the sorted list,
 				//insert it there
+				if(myNumbers[remain] < sorted.get(check))
+				{
+					inserted = true;
+					sorted.add(check, myNumbers[remain]);
+					check = remain;
+				}
 
+			}		
 			//If it was not inserted, stick it on the end.
+			if(!inserted)
+				sorted.add(myNumbers[remain]);
 
-
+			
 		//End Loop for unsorted list
+		}
 
 		System.out.println(sorted);
 
 		//Move the data back to the array
+		for(int move = 0; move < myNumbers.length; move ++)
+			myNumbers[move] = sorted.get(move);
 
 		//Print the contents of the array
+		System.out.println(Arrays.toString(myNumbers));
 
     }
 
